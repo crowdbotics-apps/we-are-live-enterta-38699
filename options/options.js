19dc518691fc4061f6f7eaa9ecc6fa39daeb8b4a
Crowdbotics <team@crowdbotics.com>
2023-02-04 00:14:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "we-are-live-enterta-38699",
+  url: "https://we-are-live-enterta-38699.botics.co",
+  api: "https://we-are-live-enterta-38699.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
